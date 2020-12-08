893299dc2719f8221cd49a17e841fdc9ee373d90
indulekha-123 <67064107+indulekha-123@users.noreply.github.com>
2020-12-08 12:18:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
+import dictionary from '../database';
+
+
+
+export default class App extends React.Component {
+    render() {
+      return (
+        <View>
+        
+        
+         </View>
+      );
+    }
+  }
